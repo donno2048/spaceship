7ed92ceacd7f4937300e7f952d130f366e08d51c
donno2048 <just4now666666@gmail.com>
2023-08-14 21:49:43 +0300

    stop looking for pointer to element of array

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ void getSize(int *x, int *y) {
     *y = w.ws_row;
 }
 #endif
-#define g(x, y, u, v, s) for (int j = 0, X = x, Y = y; j < v && Y + j < h - X / w && Y >= 0 && X >= 0; ++j) memcpy(&f[Y + j][X], &s[j * u], u)
+#define g(x, y, u, v, s) for (int j = 0, X = x, Y = y; j < v && Y + j < h - X / w && Y >= 0 && X >= 0; ++j) memcpy(&f[Y + j][X], s + j * u, u)
 #define l(x, y, w, h, a, b, c, d) !((x - a > c && x >= a) || (a - x > w && a >= x) || (y - b > d && y >= b) || (b - y > h && b >= y))
 #define f(x, y, z) p.y += z * d; strcpy(b, x); break
 int main() {
@@ -80,7 +80,7 @@ int main() {
         p.y = p.y < 2 ? 2 : p.y >= h - 3 ? h - 3 : p.y; // make sure the player is in the screen (vertically)
         for (int i = A - 1; i--;) {
             *f[h] = 0;
-            struct V *e = &a[i], *z = &m[i];
+            struct V *e = a + i, *z = m + i;
             e->x += d * z->x; // move the asteroid in the x direction
             e->y += d * z->y; // move the asteroid in the y direction
             e->x < 0 - 3 || e->x >= w + 3 || e->y >= h + 2 || g ? e->y = -rand() % h * (1 + g), e->x = rand() % w, z->x = -8 + rand() % 15, z->y = 10 + rand() % 5 : 0;
@@ -94,7 +94,7 @@ int main() {
         for (int i = -2, j = 1e7, k, K = 0; i <= 2; i++, j /= 10, k = (l - start) / j % 10, K = k | K) f[1][w / 2 + i] = K ? '0' + k : ' '; // draw the score
         getSize(&W, &H);                                                                                                                    // get the new screen size
         if (W != w || H < h) exit(printf("\033[0;31m\nPlease don't resize the screen, it will break the program\033[0m\n"));                // make sure the screen is not resized besides getting higher
-        else printf("\033[0;4H%s", &f[0][4]);                                                                                               // print the screen
+        else printf("\033[0;4H%s", f + 4);                                                                                               // print the screen
         while (GetTickCount() - l < 10);                                                                                                    // wait for a bit
         g = 0;
     }
